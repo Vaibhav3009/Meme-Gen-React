7099f189a9f71964bcc3bfd115d7d303cbd7397f
Vaibhav3009 <43685095+Vaibhav3009@users.noreply.github.com>
2020-10-14 01:34:14 +0530

    Updates

---
+++ precache-manifest.b5c32531290b167f261f0e2d050e5ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7d79f3a141c845a0e22441b89b2e5c",
+    "url": "/Meme-Gen-React/index.html"
+  },
+  {
+    "revision": "3c7d0e7121e8a9a5bb0f",
+    "url": "/Meme-Gen-React/static/css/main.98458e7f.chunk.css"
+  },
+  {
+    "revision": "21f2282ca0a6f5c7f881",
+    "url": "/Meme-Gen-React/static/js/2.2b71a7f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Gen-React/static/js/2.2b71a7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7d0e7121e8a9a5bb0f",
+    "url": "/Meme-Gen-React/static/js/main.9d76c71c.chunk.js"
+  },
+  {
+    "revision": "c0e64ba98f42b1e2382e",
+    "url": "/Meme-Gen-React/static/js/runtime-main.e1744d72.js"
+  }
+]);
